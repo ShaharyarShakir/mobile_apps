323a136a7688f64a529423469abf33033d32d3ec
ShaharyarShakir <shakirshaharyar125@gmail.com>
2026-06-02 17:22:37 +0500

    Add error handler middleware to manage and respond to errors

---
+++ gossip/backend/src/middleware/errorhandler.ts
@@ -0,0 +1,12 @@
+import type { Request, Response, NextFunction } from "express";
+
+export const errorHandler = (err: Error, _req: Request, res: Response, _next: NextFunction) => {
+	console.log("Error", err.message);
+	const statusCode = res.statusCode !== 200 ? res.statusCode : 500;
+	res.status(statusCode).json({
+		message: err.message || "Internal Server Error",
+		...(process.env.NODE_ENV === "development" && { stack: err.stack }),
+	});
+};
+
+// if status code is 200 and we still hit the error handler, it means that it's an internal server error, so we set the status code to 500
