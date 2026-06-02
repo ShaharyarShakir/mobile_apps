220d59a658c6895c506c59cb6066fb8d9627ac6f
ShaharyarShakir <shakirshaharyar125@gmail.com>
2026-06-02 17:20:20 +0500

    Initialize Express application with routing and middleware setup

---
+++ gossip/backend/src/app.ts
@@ -0,0 +1,38 @@
+import path from "node:path";
+
+import { clerkMiddleware } from "@clerk/express";
+import express from "express";
+
+import { errorHandler } from "./middleware/errorhandler";
+import authRoute from "./routes/authRoute";
+import chatRoute from "./routes/chatRoute";
+import messageRoute from "./routes/messageRoute";
+import userRoute from "./routes/userRoute";
+
+const app = express();
+
+// parse the incoming JSON request body and avail them in req.body
+app.use(express.json());
+
+app.use(clerkMiddleware());
+
+app.get("/health", (req, res) => {
+	res.json({ status: "ok", message: "Server is running" });
+});
+
+app.use("/api/auth", authRoute);
+app.use("/api/chats", chatRoute);
+app.use("/api/messages", messageRoute);
+app.use("/api/users", userRoute);
+
+// error handler middleware should be the last middleware in the stack, so it can catch all the errors that happen in the previous middlewares and routes
+app.use(errorHandler);
+// serve frontend in production
+if (process.env.NODE_ENV === "production") {
+	app.use(express.static(path.join(__dirname, "../../web/dist")));
+
+	app.get("/{*any}", (_, res) => {
+		res.sendFile(path.join(__dirname, "../../web/dist/index.html"));
+	});
+}
+export default app;
