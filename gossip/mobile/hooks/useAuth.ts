b8562eeaee8d1a47457bed673e721f8b3ee001c9
ShaharyarShakir <shakirshaharyar125@gmail.com>
2026-06-02 18:30:59 +0500

    Add useCurrentUser hook to fetch current user data

--- gossip/mobile/hooks/useAuth.ts
+++ gossip/mobile/hooks/useAuth.ts
@@ -1,6 +1,6 @@
 import { useApi } from '@/ib/axios';
 import { User } from '@sentry/react-native';
-import { useMutation } from '@tanstack/react-query';
+import { useMutation, useQuery } from '@tanstack/react-query';
 
 export const useAuthCallback = () => {
   const { apiWithAuth } = useApi();
@@ -11,6 +11,18 @@ export const useAuthCallback = () => {
         method: 'POST',
         url: '/auth/callback',
       });
+      return data;
+    },
+  });
+};
+
+export const useCurrentUser = () => {
+  const { apiWithAuth } = useApi();
+
+  return useQuery({
+    queryKey: ['currentUser'],
+    queryFn: async () => {
+      const { data } = await apiWithAuth<User>({method: 'GET', url: '/auth/me'});
       return data;
     },
   });
