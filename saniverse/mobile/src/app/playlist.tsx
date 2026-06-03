0d00b1ebf15f21f725f8fe5345595dd037bc7f07
ShaharyarShakir <shakirshaharyar125@gmail.com>
2026-06-03 18:04:12 +0500

    feat: scaffold project assets, utility hooks, and UI styles while updating playlist navigation.

--- saniverse/mobile/src/app/playlist.tsx
+++ saniverse/mobile/src/app/playlist.tsx
@@ -1,6 +1,6 @@
 import { View, Text } from 'react-native'
 import React from 'react'
-import { useRoute } from '@react-navigation/native'
+import { useLocalSearchParams } from 'expo-router'
 import { Anime } from '@/service/animeStore'
 import { commonStyles } from '@/styles/commonStyles'
 import PlaylistHeader from '@/components/playlist/PlaylistHeader'
@@ -8,8 +8,7 @@ import VideoPlayer from '@/components/playlist/VideoPlayer'
 import Interactions from '@/components/playlist/Interactions'
 
 const Page = () => {
-    const route = useRoute() as any
-    const item = route?.params as Anime
+    const item = useLocalSearchParams() as any as Anime
     return (
         <View style={commonStyles.containerBlack}>
             <PlaylistHeader title={item.title} genre={item.genre} />
