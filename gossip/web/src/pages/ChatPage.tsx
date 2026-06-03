99084803d836ffa36bbea92c7468aff9e7cbc9c9
ShaharyarShakir <shakirshaharyar125@gmail.com>
2026-06-03 09:48:20 +0500

    Add ChatPage component with basic structure

---
+++ gossip/web/src/pages/ChatPage.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function ChatPage() {
+    return (
+        <div>
+            <h1 className="text-amber-400">ChatPage</h1>
+        </div>
+    )
+}
+
+export default ChatPage
