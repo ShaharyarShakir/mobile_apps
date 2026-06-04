b083a8df295a1558b443ce586d6bb848b37e2ef8
ShaharyarShakir <shakirshaharyar125@gmail.com>
2026-06-05 01:09:01 +0500

    feat: implement authentication flow with loading state and conditional routing

--- gossip/web/src/App.tsx
+++ gossip/web/src/App.tsx
@@ -1,14 +1,20 @@
-import { Route, Routes } from "react-router"
+import { Navigate, Route, Routes } from "react-router"
 import HomePage from "./pages/HomePage"
 import ChatPage from "./pages/ChatPage"
+import { useAuth } from "@clerk/react"
+import PageLoader from "./components/PageLoader"
+import useUserSync from "./hooks/useUserSync"
+
 
 function App() {
-
+  const { isLoaded, isSignedIn } = useAuth()
+  useUserSync()
+  if (!isLoaded) return <PageLoader />
 
   return (
     <Routes>
-      <Route path="/" element={<HomePage />} />
-      <Route path="/chat" element={<ChatPage />} />
+      <Route path="/" element={!isSignedIn ? <HomePage /> : <Navigate to={'/chat'} />} />
+      <Route path="/chat" element={isSignedIn ? <ChatPage /> : <Navigate to={'/'} />} />
     </Routes>
   )
 }
