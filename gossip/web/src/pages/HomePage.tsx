612399a804aa6e9b6d0cac0023e0aa94ad16e9be
ShaharyarShakir <shakirshaharyar125@gmail.com>
2026-06-05 01:12:10 +0500

    feat: restructure HomePage layout with Navbar, Hero, and HeroImg components

--- gossip/web/src/pages/HomePage.tsx
+++ gossip/web/src/pages/HomePage.tsx
@@ -1,9 +1,17 @@
-import React from 'react'
+import Hero from "../components/Hero";
+import HeroImg from "../components/HeroImg";
+import Navbar from "../components/Navbar";
 
 export default function HomePage() {
     return (
-        <div>
-            <h1 className="text-amber-400">HomePage</h1>
+        <div className="flex bg-base-100 h-screen text-base">
+            {/* LEFT SIDE */}
+            <div className="relative flex flex-col flex-1 p-8 lg:p-12 overflow-hidden">
+                <Navbar />
+                <Hero />
+            </div>
+
+            <HeroImg />
         </div>
     )
 }
