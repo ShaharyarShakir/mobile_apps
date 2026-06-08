6aed4bf9b3d4d79602b248c89b42548366f23548
ShaharyarShakir <shakirshaharyar125@gmail.com>
2026-06-08 08:53:56 +0500

    feat: enhance authentication flow by adding protected routes and refactoring layout structure

--- pocket-app/src/app/_layout.tsx
+++ pocket-app/src/app/_layout.tsx
@@ -1,19 +1,26 @@
+import { ClerkProvider, useAuth } from '@clerk/expo'
+import { tokenCache } from '@clerk/expo/token-cache'
 import { Stack } from "expo-router"
 import { KeyboardProvider } from "react-native-keyboard-controller"
-
+const publishableKey = process.env.EXPO_PUBLIC_CLERK_PUBLISHABLE_KEY!
 const Layout = () => {
+    const { isSignedIn } = useAuth()
     return <Stack>
-        <Stack.Screen name="index" options={{
-            headerShown: false
-        }} />
+        <Stack.Protected guard={!isSignedIn}>
+            <Stack.Screen name="index" options={{ headerShown: false }} />
+        </Stack.Protected>
         <Stack.Screen name="(tabs)" options={{
             headerShown: false
         }} />
     </Stack>
 }
 const RootLayout = () => {
-    return <KeyboardProvider>
-        <Layout />
-    </KeyboardProvider>
+    return (
+        <ClerkProvider publishableKey={publishableKey} tokenCache={tokenCache}>
+            <KeyboardProvider>
+                <Layout />
+            </KeyboardProvider>
+        </ClerkProvider>
+    )
 }
-export default Layout
+export default RootLayout
