56db6243e82ab4d585289a39ebc10cc8420812a9
ShaharyarShakir <shakirshaharyar125@gmail.com>
2026-06-08 08:54:11 +0500

    feat: add SettingsLayout component for Saves screen

---
+++ pocket-app/src/app/(tabs)/saves/_layout.tsx
@@ -0,0 +1,23 @@
+import { COLORS } from '@/utils/Colors';
+import { Stack } from 'expo-router';
+
+export default function SettingsLayout() {
+    return (
+        <Stack
+            screenOptions={{
+                contentStyle: {
+                    backgroundColor: COLORS.white,
+                },
+            }}>
+            <Stack.Screen
+                name="index"
+                options={{
+                    title: 'Saves',
+                    headerLargeTitle: true,
+                    headerLargeTitleShadowVisible: false,
+
+                }}
+            />
+        </Stack>
+    );
+}
