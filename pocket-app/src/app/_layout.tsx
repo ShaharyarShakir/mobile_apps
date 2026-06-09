1817f672c25b3071845aa0c5229935d65ae38780
ShaharyarShakir <shakirshaharyar125@gmail.com>
2026-06-09 14:02:03 +0500

    feat: refactor database handling in layout component

--- pocket-app/src/app/_layout.tsx
+++ pocket-app/src/app/_layout.tsx
@@ -1,6 +1,7 @@
 import migrations from '@/drizzle/migrations'
 import { ClerkProvider, useAuth } from '@clerk/expo'
 import { tokenCache } from '@clerk/expo/token-cache'
+import { drizzle } from 'drizzle-orm/expo-sqlite'
 import { useMigrations } from 'drizzle-orm/expo-sqlite/migrator'
 import { useDrizzleStudio } from 'expo-drizzle-studio-plugin'
 import { Stack } from "expo-router"
@@ -13,8 +14,8 @@ const publishableKey = process.env.EXPO_PUBLIC_CLERK_PUBLISHABLE_KEY!
 const DATABSE_NAME = "pocket"
 const Layout = () => {
     const { isSignedIn } = useAuth()
-    const db = openDatabaseSync(DATABSE_NAME)
-    useDrizzleStudio(db)
+    // const db = openDatabaseSync(DATABSE_NAME)
+
     return <Stack>
         <Stack.Protected guard={!isSignedIn}>
             <Stack.Screen name="index" options={{ headerShown: false }} />
@@ -26,7 +27,12 @@ const Layout = () => {
 }
 const RootLayout = () => {
     const expoDb = openDatabaseSync(DATABSE_NAME)
-    const { success, error } = useMigrations(expoDb, migrations)
+    // console.log('migrations =', migrations);
+    const db = drizzle(expoDb)
+    useDrizzleStudio(expoDb)
+    const { success, error } = useMigrations(db, migrations)
+    // console.log('🚀 ~ RootLayout ~ error:', error);
+    // console.log('🚀 ~ RootLayout ~ success:', success);
     return (
         <ClerkProvider publishableKey={publishableKey} tokenCache={tokenCache}>
             <KeyboardProvider>
