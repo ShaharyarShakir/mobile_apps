90535c39622d5922fa9611dcd7d3ec540768bcc0
ShaharyarShakir <shakirshaharyar125@gmail.com>
2026-06-09 10:25:43 +0500

    feat: integrate SQLite provider and migration handling in layout component

--- pocket-app/src/app/_layout.tsx
+++ pocket-app/src/app/_layout.tsx
@@ -1,10 +1,20 @@
+import migrations from '@/drizzle/migrations'
 import { ClerkProvider, useAuth } from '@clerk/expo'
 import { tokenCache } from '@clerk/expo/token-cache'
+import { useMigrations } from 'drizzle-orm/expo-sqlite/migrator'
+import { useDrizzleStudio } from 'expo-drizzle-studio-plugin'
 import { Stack } from "expo-router"
+import { openDatabaseSync, SQLiteProvider } from 'expo-sqlite'
+import { Suspense } from 'react'
+import { ActivityIndicator } from 'react-native'
 import { KeyboardProvider } from "react-native-keyboard-controller"
 const publishableKey = process.env.EXPO_PUBLIC_CLERK_PUBLISHABLE_KEY!
+
+const DATABSE_NAME = "pocket"
 const Layout = () => {
     const { isSignedIn } = useAuth()
+    const db = openDatabaseSync(DATABSE_NAME)
+    useDrizzleStudio(db)
     return <Stack>
         <Stack.Protected guard={!isSignedIn}>
             <Stack.Screen name="index" options={{ headerShown: false }} />
@@ -15,10 +25,17 @@ const Layout = () => {
     </Stack>
 }
 const RootLayout = () => {
+    const expoDb = openDatabaseSync(DATABSE_NAME)
+    const { success, error } = useMigrations(expoDb, migrations)
     return (
         <ClerkProvider publishableKey={publishableKey} tokenCache={tokenCache}>
             <KeyboardProvider>
-                <Layout />
+                <Suspense fallback={<ActivityIndicator />} >
+                    <SQLiteProvider databaseName={DATABSE_NAME} options={{ enableChangeListener: true }}>
+                        <Layout />
+                    </SQLiteProvider>
+                </Suspense>
+
             </KeyboardProvider>
         </ClerkProvider>
     )
