f708b7ec50fab42463e93a4450f5f5a27a4a2c8e
ShaharyarShakir <shakirshaharyar125@gmail.com>
2026-06-11 14:22:36 +0500

    feat: replace success message with SavedConfirmation component for enhanced user experience

--- pocket-app/src/app/(modal)success.tsx
+++ pocket-app/src/app/(modal)success.tsx
@@ -1,9 +1,27 @@
-import { Text, View } from 'react-native'
+import SavedConfirmation from '@/components/saved-confirmation';
+import { COLORS } from '@/utils/Colors';
+import { useRouter } from 'expo-router';
+import { StyleSheet, View } from 'react-native';
+const Page = () => {
+    const router = useRouter();
 
-export default function Page() {
+    const handleCancel = () => {
+        router.dismiss();
+    };
+
     return (
-        <View>
-            <Text>Success</Text>
+        <View style={styles.container}>
+            <SavedConfirmation onDismiss={handleCancel} />
         </View>
-    )
-}
+    );
+};
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        backgroundColor: COLORS.white,
+        height: '100%',
+    },
+});
+
+export default Page;
