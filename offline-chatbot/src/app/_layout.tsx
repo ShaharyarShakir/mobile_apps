9ed2029e00006976a67371325ed258ef6f9327e2
ShaharyarShakir <shakirshaharyar125@gmail.com>
2026-06-19 16:48:01 +0500

    feat: add RootLayout component with theme support and navigation stack

---
+++ offline-chatbot/src/app/_layout.tsx
@@ -0,0 +1,21 @@
+import "../global.css";
+
+import { NAV_THEME } from "@/lib/theme";
+import { PortalHost } from "@rn-primitives/portal";
+import { Stack, ThemeProvider } from "expo-router";
+import { StatusBar } from "expo-status-bar";
+
+export default function RootLayout() {
+  return (
+    <ThemeProvider value={NAV_THEME.dark}>
+      <Stack>
+        <Stack.Screen
+          name="index"
+          options={{ title: "Home", headerShown: false }}
+        />
+      </Stack>
+      <PortalHost />
+      <StatusBar style="light" />
+    </ThemeProvider>
+  );
+}
