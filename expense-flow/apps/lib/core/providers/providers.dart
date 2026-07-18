e5c93cfadf4e933a44e53eb6d9bb73e20b3c6272
ShaharyarShakir <shakirshaharyar125@gmail.com>
2026-07-18 19:15:05 +0500

    feat: refactor providers to integrate secure storage and network client

--- expense-flow/apps/lib/core/providers/providers.dart
+++ expense-flow/apps/lib/core/providers/providers.dart
@@ -1,11 +1,27 @@
-import 'package:api_client/api_client.dart';
+import 'package:flutter/foundation.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
-import '../config/app_config.dart';
+import 'package:flutter_secure_storage/flutter_secure_storage.dart';
+import '../database/local_database.dart';
+import '../network/network_client.dart';
+import '../storage/secure_storage.dart';
 
-final apiClientProvider = Provider<ApiClient>((ref) {
-  return ApiClient(baseUrl: AppConfig.apiUrl);
+final secureStorageProvider = Provider<SecureStorageService>((ref) {
+  return SecureStorageService(const FlutterSecureStorage());
 });
 
-final authApiProvider = Provider<AuthApi>((ref) {
-  return AuthApi(ref.watch(apiClientProvider));
+final databaseProvider = Provider<AppDatabase>((ref) {
+  final db = AppDatabase();
+  ref.onDispose(db.close);
+  return db;
+});
+
+final networkClientProvider = Provider<NetworkClient>((ref) {
+  final baseUrl = defaultTargetPlatform == TargetPlatform.android
+      ? 'http://10.0.2.2:8080'
+      : 'http://localhost:8080';
+
+  return NetworkClient(
+    baseUrl: baseUrl,
+    secureStorage: ref.read(secureStorageProvider),
+  );
 });
