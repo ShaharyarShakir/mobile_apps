6486828dbefb5876701fe9cd8cf08d4604a70eca
ShaharyarShakir <shakirshaharyar125@gmail.com>
2026-07-18 19:14:58 +0500

    feat: implement NetworkClient for handling network requests with Dio

---
+++ expense-flow/apps/lib/core/network/network_client.dart
@@ -0,0 +1,97 @@
+import 'package:dio/dio.dart';
+import '../storage/secure_storage.dart';
+import 'auth_interceptor.dart';
+import 'logging_interceptor.dart';
+
+class NetworkClient {
+  final Dio _dio;
+
+  NetworkClient({
+    required String baseUrl,
+    required SecureStorageService secureStorage,
+  }) : _dio = Dio(
+          BaseOptions(
+            baseUrl: baseUrl,
+            connectTimeout: const Duration(seconds: 15),
+            receiveTimeout: const Duration(seconds: 15),
+            headers: {
+              'Content-Type': 'application/json',
+              'Accept': 'application/json',
+            },
+          ),
+        ) {
+    final refreshDio = Dio(
+      BaseOptions(
+        baseUrl: baseUrl,
+        connectTimeout: const Duration(seconds: 15),
+        receiveTimeout: const Duration(seconds: 15),
+        headers: {
+          'Content-Type': 'application/json',
+          'Accept': 'application/json',
+        },
+      ),
+    );
+    refreshDio.interceptors.add(LoggingInterceptor());
+
+    _dio.interceptors.addAll([
+      AuthInterceptor(secureStorage, refreshDio),
+      LoggingInterceptor(),
+    ]);
+  }
+
+  Dio get dio => _dio;
+
+  Future<Response<T>> get<T>(
+    String path, {
+    Map<String, dynamic>? queryParameters,
+    Options? options,
+  }) {
+    return _dio.get<T>(
+      path,
+      queryParameters: queryParameters,
+      options: options,
+    );
+  }
+
+  Future<Response<T>> post<T>(
+    String path, {
+    dynamic data,
+    Map<String, dynamic>? queryParameters,
+    Options? options,
+  }) {
+    return _dio.post<T>(
+      path,
+      data: data,
+      queryParameters: queryParameters,
+      options: options,
+    );
+  }
+
+  Future<Response<T>> put<T>(
+    String path, {
+    dynamic data,
+    Map<String, dynamic>? queryParameters,
+    Options? options,
+  }) {
+    return _dio.put<T>(
+      path,
+      data: data,
+      queryParameters: queryParameters,
+      options: options,
+    );
+  }
+
+  Future<Response<T>> delete<T>(
+    String path, {
+    dynamic data,
+    Map<String, dynamic>? queryParameters,
+    Options? options,
+  }) {
+    return _dio.delete<T>(
+      path,
+      data: data,
+      queryParameters: queryParameters,
+      options: options,
+    );
+  }
+}
