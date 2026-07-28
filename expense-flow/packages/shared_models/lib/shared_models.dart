1998bb35048b5f5954bf269f18214f0f83c75894
ShaharyarShakir <shakirshaharyar125@gmail.com>
2026-07-29 00:45:24 +0500

    feat: create shared_models library and export necessary modules

---
+++ expense-flow/packages/shared_models/lib/shared_models.dart
@@ -0,0 +1,11 @@
+library shared_models;
+
+export 'src/common/api_error.dart';
+export 'src/common/api_response.dart';
+
+export 'src/auth/user.dart';
+export 'src/auth/auth_response.dart';
+
+export 'src/expense/category.dart';
+export 'src/expense/expense.dart';
+export 'src/dashboard/dashboard_models.dart';
