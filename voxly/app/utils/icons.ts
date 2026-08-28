6dd84c4d431b22051b6e5fa03de141a91c39009c
ShaharyarShakir <shakirshaharyar125@gmail.com>
2026-08-28 10:56:22 +0500

    feat: add Font Awesome 5 Free Unicode mapping constants for NativeScript

---
+++ voxly/app/utils/icons.ts
@@ -0,0 +1,22 @@
+// Font Awesome 5 Free Unicode mapping constants for NativeScript
+export const ICONS = {
+  MIC: '\uf130',
+  STOP: '\uf04d',
+  PLAY: '\uf04b',
+  PAUSE: '\uf04c',
+  COG: '\uf013',
+  SEARCH: '\uf002',
+  ELLIPSIS: '\uf141',
+  LIGHTBULB: '\uf0eb',
+  WAVE: '\uf028',
+  FILE_TEXT: '\uf15c',
+  BOLT: '\uf0e7',
+  PLUS: '\uf067',
+  CHECK: '\uf00c',
+  SPARKLES: '\uf005',
+  TRASH: '\uf1f8',
+  TAG: '\uf02b',
+  TIMES: '\uf00d',
+  EDIT: '\uf044',
+  HEART: '\uf004'
+} as const;
