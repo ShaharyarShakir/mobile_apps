4c55b421a2e9d287100a80463dcf6147fc5e70a6
ShaharyarShakir <shakirshaharyar125@gmail.com>
2026-08-28 10:33:08 +0500

    feat: add svelte.config.js for Svelte preprocessing configuration

---
+++ voxly/svelte.config.js
@@ -0,0 +1,31 @@
+const sveltePreprocess = require('svelte-preprocess')
+const svelteNativePreprocessor = require('@nativescript-community/svelte-native-preprocessor')
+
+// this can be called either through svelte-loader where we want either __ANDROID__ or __IOS__ to be defined but not both
+// or through svelte-check where we want both so everything is checked
+const webpack_env = process.env['NATIVESCRIPT_WEBPACK_ENV']
+  ? JSON.parse(process.env['NATIVESCRIPT_WEBPACK_ENV'])
+  : {
+      android: true,
+      ios: true,
+    }
+module.exports = {
+  compilerOptions: {
+    namespace: 'foreign',
+  },
+  preprocess: [
+    sveltePreprocess({
+      replace: [
+        [/__ANDROID__/g, !!webpack_env.android],
+        [/__IOS__/g, !!webpack_env.ios],
+      ],
+      typescript: {
+        compilerOptions: {
+          verbatimModuleSyntax: true,
+          target: 'es2020',
+        },
+      },
+    }),
+  ],
+}
+
