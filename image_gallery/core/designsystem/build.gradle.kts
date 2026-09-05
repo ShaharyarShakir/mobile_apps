3e27e794328b10db2f316898d5be85739dc02412
ShaharyarShakir <shakirshaharyar125@gmail.com>
2026-09-05 23:50:36 +0500

    feat: add build.gradle.kts for design system module configuration

---
+++ image_gallery/core/designsystem/build.gradle.kts
@@ -0,0 +1,36 @@
+plugins {
+    alias(libs.plugins.android.library)
+    alias(libs.plugins.kotlin.android)
+    alias(libs.plugins.kotlin.compose)
+}
+
+android {
+    namespace = "com.example.imagegallery.core.designsystem"
+
+    compileSdk = 36
+
+    defaultConfig {
+        minSdk = 24
+    }
+
+    compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_17
+        targetCompatibility = JavaVersion.VERSION_17
+    }
+
+    kotlinOptions {
+        jvmTarget = "17"
+    }
+
+    buildFeatures {
+        compose = true
+    }
+}
+
+dependencies {
+    implementation(platform(libs.composeBom))
+
+    implementation(libs.composeUi)
+    implementation(libs.composeRuntime)
+    implementation(libs.composeMaterial3)
+}
