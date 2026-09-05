fdb880316ae0497e611560aa54ce9417230b46bf
ShaharyarShakir <shakirshaharyar125@gmail.com>
2026-09-05 23:50:47 +0500

    feat: add Color.kt for design system color definitions

---
+++ image_gallery/core/designsystem/src/main/kotlin/com/example/imagegallery/core/designsystem/Color.kt
@@ -0,0 +1,9 @@
+package com.example.imagegallery.core.designsystem
+
+import androidx.compose.ui.graphics.Color
+
+val GalleryPrimary = Color(0xFF6750A4)
+val GalleryOnPrimary = Color(0xFFFFFFFF)
+
+val GalleryLightBackground = Color(0xFFFFFBFE)
+val GalleryDarkBackground = Color(0xFF1C1B1F)
