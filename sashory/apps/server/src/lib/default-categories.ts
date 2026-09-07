30612dab02ac4497e357a4d4b5cdf4d9c6028a62
ShaharyarShakir <shakirshaharyar125@gmail.com>
2026-09-07 06:32:44 +0500

    feat: add DEFAULT_CATEGORIES constant for expense and income categorization

---
+++ sashory/apps/server/src/lib/default-categories.ts
@@ -0,0 +1,47 @@
+export const DEFAULT_CATEGORIES = [
+  {
+    name: "Food",
+    type: "EXPENSE",
+  },
+  {
+    name: "Transport",
+    type: "EXPENSE",
+  },
+  {
+    name: "Rent",
+    type: "EXPENSE",
+  },
+  {
+    name: "Utilities",
+    type: "EXPENSE",
+  },
+  {
+    name: "Shopping",
+    type: "EXPENSE",
+  },
+  {
+    name: "Healthcare",
+    type: "EXPENSE",
+  },
+  {
+    name: "Entertainment",
+    type: "EXPENSE",
+  },
+  {
+    name: "Salary",
+    type: "INCOME",
+  },
+  {
+    name: "Freelance",
+    type: "INCOME",
+  },
+  {
+    name: "Business",
+    type: "INCOME",
+  },
+  {
+    name: "Other Income",
+    type: "INCOME",
+  },
+] as const;
+
